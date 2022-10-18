c57e1129ea1f620beaf5df20a2bb7d30397bad0d
roji-tech <rojitech9@gmail.com>
2022-10-18 21:47:27 +0100

    a

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, filter
 
 urlpatterns = [
     path('', index, name="index"),
+    path('filter/', filter, name="filter"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    context = {
+        "status": "",
+    }
+    return render(request, "index.html", context)
+    # return render(request, "index.html", context, content_type, status, using)
+
+
+# Create your views here.
+def filter(request):
     print(request.POST)
+    print("GOT HERE")
     filter_option = request.POST.get("filter_opt")
     myfile = request.FILES.get("file")
 
