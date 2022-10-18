d92ba05235a66f9f3aad1224f1c9e60c2c065f83
roji-tech <rojitech9@gmail.com>
2022-10-18 21:38:34 +0100

    advance

--- main/views.py
+++ main/views.py
@@ -3,15 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-  print(request.POST)
-  filter_option = request.POST.get("filter_opt")
-  file = request.FILES.get("file")
+    print(request.POST)
+    filter_option = request.POST.get("filter_opt")
+    myfile = request.FILES.get("file")
 
-  print(filter_option)
-  print(file.name)
+    print(filter_option)
+    # print(myfile.name)
 
-  context = {
-    "status": "",
-  }
-  return render(request, "index.html", context)
-  # return render(request, "index.html", context, content_type, status, using)
+    context = {
+        "status": "",
+    }
+    return render(request, "index.html", context)
+    # return render(request, "index.html", context, content_type, status, using)
